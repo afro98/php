49d714d4cc1a8d011edb52d9c71a791a98623e0a
afro98 <eliaspablo795@gmail.com>
2023-11-24 14:36:57 -0300

    cambios24112023

---
+++ rand.php
@@ -0,0 +1,26 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+$valor = rand(1,5);
+
+
+if($valor == 1 || $valor == 3 || $valor == 5){
+    echo "$valor es par";
+} else {
+    "$valor es impar";
+}
+
+
+
+
+if($valor % 2 == 0){ //Analiza el resto de dividir por 2, todo número divisible por 2 es par
+    echo "$valor es par";
+} else {
+    echo "$valor es impar";
+}
+
+?>
