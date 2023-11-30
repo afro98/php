04b6edb792dd2aed0d96e69c6cfb4e2648cf55e8
afro98 <eliaspablo795@gmail.com>
2023-11-29 21:14:41 -0300

    config inicial

---
+++ numeros_pares.php
@@ -0,0 +1,17 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+for($numero = 2; $numero <= 100; $numero+=2){
+
+    if($numero == 62){
+        break;
+    }
+
+    echo "$numero <br>";
+
+}
+
+?>
