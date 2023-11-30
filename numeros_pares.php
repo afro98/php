9041db80c50f44e4a2ee5fd295227a417c0472cf
afro98 <eliaspablo795@gmail.com>
2023-11-29 21:17:23 -0300

    cambios29112023

--- numeros_pares.php
+++ numeros_pares.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-for($numero = 2; $numero <= 100; $numero+=2){
+for($numero = 2; $numero <= 100; $numero += 2){
 
     if($numero == 62){
         break;
